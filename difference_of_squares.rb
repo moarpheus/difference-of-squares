5e59ff1a56bbd816b7520ea20da1f4f51d50903e
Juris Gribuska <juris.gribuska@bt.com>
2021-06-17 10:18:21 +0300

    complete

--- difference_of_squares.rb
+++ difference_of_squares.rb
@@ -1,8 +1,18 @@
 class Squares
   def initialize number
     @square_of_sum = (1..number).inject(&:+)**2
+    @sum_of_squares = (1..number).map {|num| num ** 2}.inject(&:+)
   end
+
   def square_of_sum
     @square_of_sum    
+  end
+
+  def sum_of_squares
+    @sum_of_squares
+  end
+
+  def difference
+    @square_of_sum - @sum_of_squares
   end
 end
