fe61ba5eca8fb6d2c3e6028946df2ca97b8eaf7b
Juris Gribuska <juris.gribuska@bt.com>
2021-06-17 10:24:06 +0300

    refactoring

--- difference_of_squares.rb
+++ difference_of_squares.rb
@@ -1,18 +1,17 @@
 class Squares
   def initialize number
-    @square_of_sum = (1..number).inject(&:+)**2
-    @sum_of_squares = (1..number).map {|num| num ** 2}.inject(&:+)
+    @number = number
   end
 
   def square_of_sum
-    @square_of_sum    
+    (1..@number).inject(&:+)**2
   end
 
   def sum_of_squares
-    @sum_of_squares
+    (1..@number).map {|num| num ** 2}.inject(&:+)
   end
 
   def difference
-    @square_of_sum - @sum_of_squares
+    square_of_sum - sum_of_squares
   end
 end
